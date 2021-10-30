141be18cf42beb2fa9363a37256abbe1cca8b981
Parinya Samrongsap <zabahey.sukoi@gmail.com>
2021-10-30 22:06:05 +0700

    send method to response

--- api/middleware/authenticate.js
+++ api/middleware/authenticate.js
@@ -3,23 +3,29 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try {
     if (req.headers.authorization === undefined) {
-      return res.status(401).json({
-        error: {
-          code: 401,
-          message: 'UnAuthorized',
-        },
-      });
+      return res
+        .status(401)
+        .json({
+          error: {
+            code: 401,
+            message: 'UnAuthorized',
+          },
+        })
+        .send();
     }
     const token = req.headers.authorization.split(' ')[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
     req.user = decoded;
     next();
   } catch (error) {
-    return res.status(401).json({
-      error: {
-        code: 401,
-        message: 'UnAuthorized',
-      },
-    });
+    return res
+      .status(401)
+      .json({
+        error: {
+          code: 401,
+          message: 'UnAuthorized',
+        },
+      })
+      .send();
   }
 };
