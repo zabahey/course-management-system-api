0fda10e764f5aad844d6fcbf644d30eb611819c9
Parinya Samrongsap <zabahey.sukoi@gmail.com>
2021-10-30 20:30:16 +0700

    Test return for create course

--- api/routes/course.js
+++ api/routes/course.js
@@ -20,52 +20,57 @@ router.get('/', CoursesController.getCourses);
 router.post(
   '/',
   authenticate,
-  authorize(Role.Instructor),
-  awsImageService.upload.single('image'),
-  body('name').notEmpty().withMessage('Name is required'),
-  body('description').notEmpty().withMessage('Description is required'),
-  body('category').notEmpty().withMessage('Category is required'),
-  body('subject').notEmpty().withMessage('Subject is required'),
-  body('startDate')
-    .notEmpty()
-    .withMessage('Start date is required')
-    .isNumeric()
-    .withMessage('Start date must be unix epoch timestamp in millisecond')
-    .custom((value) => {
-      const startDate = new Date(+value);
-      return !isNaN(startDate);
-    })
-    .withMessage('Start date is invalida value')
-    .custom((value, { req, location, path }) => {
-      const { endDate } = req.body;
-      return !endDate || +value < +endDate;
-    })
-    .withMessage('Start date must before end date'),
-  body('endDate')
-    .notEmpty()
-    .withMessage('End date is required')
-    .isNumeric()
-    .withMessage('Start date must be unix epoch timestamp in millisecond')
-    .custom((value) => {
-      const endDate = new Date(+value);
-      return !isNaN(endDate);
-    })
-    .withMessage('End date is invalida value')
-    .custom((value, { req, location, path }) => {
-      const { startDate } = req.body;
-      return !startDate || +value >= +startDate;
-    })
-    .withMessage('End date must after start date'),
-  body('numberOfStudent')
-    .notEmpty()
-    .withMessage('Number of student is required')
-    .isNumeric()
-    .withMessage('Number of student must be number')
-    .isInt({ min: 1 })
-    .withMessage('Number of student must more than 0(zero)'),
-  deleteImageWhenClientError,
-  validateClientError,
-  CoursesController.createNewCourse
+  (req, res, next) => {
+    res.status(401).json({
+      message: 'create course',
+    });
+  }
+  // authorize(Role.Instructor),
+  // awsImageService.upload.single('image'),
+  // body('name').notEmpty().withMessage('Name is required'),
+  // body('description').notEmpty().withMessage('Description is required'),
+  // body('category').notEmpty().withMessage('Category is required'),
+  // body('subject').notEmpty().withMessage('Subject is required'),
+  // body('startDate')
+  //   .notEmpty()
+  //   .withMessage('Start date is required')
+  //   .isNumeric()
+  //   .withMessage('Start date must be unix epoch timestamp in millisecond')
+  //   .custom((value) => {
+  //     const startDate = new Date(+value);
+  //     return !isNaN(startDate);
+  //   })
+  //   .withMessage('Start date is invalida value')
+  //   .custom((value, { req, location, path }) => {
+  //     const { endDate } = req.body;
+  //     return !endDate || +value < +endDate;
+  //   })
+  //   .withMessage('Start date must before end date'),
+  // body('endDate')
+  //   .notEmpty()
+  //   .withMessage('End date is required')
+  //   .isNumeric()
+  //   .withMessage('Start date must be unix epoch timestamp in millisecond')
+  //   .custom((value) => {
+  //     const endDate = new Date(+value);
+  //     return !isNaN(endDate);
+  //   })
+  //   .withMessage('End date is invalida value')
+  //   .custom((value, { req, location, path }) => {
+  //     const { startDate } = req.body;
+  //     return !startDate || +value >= +startDate;
+  //   })
+  //   .withMessage('End date must after start date'),
+  // body('numberOfStudent')
+  //   .notEmpty()
+  //   .withMessage('Number of student is required')
+  //   .isNumeric()
+  //   .withMessage('Number of student must be number')
+  //   .isInt({ min: 1 })
+  //   .withMessage('Number of student must more than 0(zero)'),
+  // deleteImageWhenClientError,
+  // validateClientError,
+  // CoursesController.createNewCourse
 );
 
 router.get(
