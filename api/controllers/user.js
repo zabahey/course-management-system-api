3f30049f50d7dcbaf3b9206fc7d0cc1afd17cf58
Parinya Samrongsap <zabahey.sukoi@gmail.com>
2021-10-30 13:51:11 +0700

    Add user login

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const { validationResult } = require('express-validator');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
 const salt = +process.env.BCRYPT_SALT_OR_ROUND;
 
 exports.signup = async (req, res, next) => {
@@ -29,7 +30,47 @@ exports.signup = async (req, res, next) => {
 };
 
 exports.login = async (req, res, next) => {
-  res.status(200).json({
-    message: 'login',
-  });
+  const { username, password } = req.body;
+  const users = await User.find({ username: username });
+  if (users.length < 1) {
+    return res.status(401).json({
+      code: 401,
+      message: 'Authentication failed',
+    });
+  }
+
+  try {
+    const currentUser = users[0];
+    const isValidPassword = await bcrypt.compare(
+      password,
+      currentUser.password
+    );
+    if (!isValidPassword) {
+      return res.status(401).json({
+        code: 401,
+        message: 'Authentication failed',
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        username: currentUser.username,
+        role: currentUser.role,
+      },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: '1h',
+        subject: currentUser._id.toString(),
+      }
+    );
+    res.status(200).json({
+      coed: 200,
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({
+      code: 500,
+      message: error.message,
+    });
+  }
 };
