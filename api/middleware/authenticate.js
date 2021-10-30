9499a624bd6122089190030d43ff8a7d98ed8983
Parinya Samrongsap <zabahey.sukoi@gmail.com>
2021-10-30 20:05:43 +0700

    Prevent error when no authorization header

--- api/middleware/authenticate.js
+++ api/middleware/authenticate.js
@@ -2,16 +2,25 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   try {
+    if (!req.headers.authorization) {
+      return res.status(401).json({
+        error: {
+          code: 401,
+          message: 'UnAuthorized',
+        },
+      });
+    }
     const token = req.headers.authorization.split(' ')[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
     req.user = decoded;
     next();
   } catch (error) {
+    console.log(error);
     return res.status(401).json({
       error: {
         code: 401,
         message: 'UnAuthorized',
-      }
+      },
     });
   }
 };
