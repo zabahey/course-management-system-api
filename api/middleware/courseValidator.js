7e7fea5f3153f3c7010fcf93e8b172de2fedb198
Parinya Samrongsap <zabahey.sukoi@gmail.com>
2021-10-31 09:31:20 +0700

    Extract course validator middleware to its own file

---
+++ api/middleware/courseValidator.js
@@ -0,0 +1,63 @@
+const { body } = require('express-validator');
+
+const authenticate = require('./authenticate');
+const authorize = require('./authorize');
+const validateClientError = require('./validate-client-error');
+const deleteImageWhenClientError = require('./delete-image-client-error');
+
+const awsImageService = require('../services/aws-image');
+
+const Role = require('../models/role');
+
+const courseValidator = () => {
+  return [
+    authenticate,
+    authorize(Role.Instructor),
+    awsImageService.upload.single('image'),
+    body('name').notEmpty().withMessage('Name is required'),
+    body('description').notEmpty().withMessage('Description is required'),
+    body('category').notEmpty().withMessage('Category is required'),
+    body('subject').notEmpty().withMessage('Subject is required'),
+    body('startDate')
+      .notEmpty()
+      .withMessage('Start date is required')
+      .isNumeric()
+      .withMessage('Start date must be unix epoch timestamp in millisecond')
+      .custom((value) => {
+        const startDate = new Date(+value);
+        return !isNaN(startDate);
+      })
+      .withMessage('Start date is invalida value')
+      .custom((value, { req, location, path }) => {
+        const { endDate } = req.body;
+        return !endDate || +value < +endDate;
+      })
+      .withMessage('Start date must before end date'),
+    body('endDate')
+      .notEmpty()
+      .withMessage('End date is required')
+      .isNumeric()
+      .withMessage('Start date must be unix epoch timestamp in millisecond')
+      .custom((value) => {
+        const endDate = new Date(+value);
+        return !isNaN(endDate);
+      })
+      .withMessage('End date is invalida value')
+      .custom((value, { req, location, path }) => {
+        const { startDate } = req.body;
+        return !startDate || +value >= +startDate;
+      })
+      .withMessage('End date must after start date'),
+    body('numberOfStudent')
+      .notEmpty()
+      .withMessage('Number of student is required')
+      .isNumeric()
+      .withMessage('Number of student must be number')
+      .isInt({ min: 1 })
+      .withMessage('Number of student must more than 0(zero)'),
+    deleteImageWhenClientError,
+    validateClientError,
+  ];
+};
+
+module.exports = courseValidator;
--- api/routes/course.js
+++ api/routes/course.js
@@ -3,71 +3,21 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Types.ObjectId;
 
-const { body, param } = require('express-validator');
+const { param } = require('express-validator');
 
+// middleware
 const validateClientError = require('../middleware/validate-client-error');
-const deleteImageWhenClientError = require('../middleware/delete-image-client-error');
 const authenticate = require('../middleware/authenticate');
-const authorize = require('../middleware/authorize');
+const courseValidator = require('../middleware/courseValidator');
 
 const CoursesController = require('../controllers/courses');
-const Role = require('../models/role');
 
-const awsImageService = require('../services/aws-image');
-const { json } = require('express');
 
 router.get('/', authenticate, CoursesController.getCourses);
 
-const upload = awsImageService.upload.single('image');
-
 router.post(
   '/',
-  authenticate,
-  authorize(Role.Instructor),
-  awsImageService.upload.single('image'),
-  body('name').notEmpty().withMessage('Name is required'),
-  body('description').notEmpty().withMessage('Description is required'),
-  body('category').notEmpty().withMessage('Category is required'),
-  body('subject').notEmpty().withMessage('Subject is required'),
-  body('startDate')
-    .notEmpty()
-    .withMessage('Start date is required')
-    .isNumeric()
-    .withMessage('Start date must be unix epoch timestamp in millisecond')
-    .custom((value) => {
-      const startDate = new Date(+value);
-      return !isNaN(startDate);
-    })
-    .withMessage('Start date is invalida value')
-    .custom((value, { req, location, path }) => {
-      const { endDate } = req.body;
-      return !endDate || +value < +endDate;
-    })
-    .withMessage('Start date must before end date'),
-  body('endDate')
-    .notEmpty()
-    .withMessage('End date is required')
-    .isNumeric()
-    .withMessage('Start date must be unix epoch timestamp in millisecond')
-    .custom((value) => {
-      const endDate = new Date(+value);
-      return !isNaN(endDate);
-    })
-    .withMessage('End date is invalida value')
-    .custom((value, { req, location, path }) => {
-      const { startDate } = req.body;
-      return !startDate || +value >= +startDate;
-    })
-    .withMessage('End date must after start date'),
-  body('numberOfStudent')
-    .notEmpty()
-    .withMessage('Number of student is required')
-    .isNumeric()
-    .withMessage('Number of student must be number')
-    .isInt({ min: 1 })
-    .withMessage('Number of student must more than 0(zero)'),
-  deleteImageWhenClientError,
-  validateClientError,
+  courseValidator(),
   CoursesController.createNewCourse
 );
 
@@ -83,5 +33,6 @@ router.get(
   validateClientError,
   CoursesController.getCourseById
 );
+
 
 module.exports = router;
